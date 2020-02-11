24c5af6ab8ac1d9db97aa5368971046d918a9962
abqkatrina <abqkatrina@gmail.com>
2020-02-11 16:52:06 -0700

    working on followers

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -12,24 +12,39 @@ class App extends Component {
     this.state = {
       query: '',
       user: [],
+      follower: {},
       followers: []
     };
   };
-
+ //bca9297f4c26404e5baf081296d223644aee6f9a
   componentDidMount(){
-    axios.get('https://api.github.com/users/abqkatrina')
-    .then(response => {
-      console.log('cdm response', response.data);
-      this.setState({user: response.data})
-    })
-    .catch(error => { console.log('cdm whoops', error)})
+    axios.get('https://api.github.com/rate_limit')
+    .then(response => {console.log(response)})
+
+    axios.get('https://api.github.com/users/abqkatrina', )
+      .then(response => {
+        // console.log('cdm response', response.data);
+        this.setState({user: response.data})
+      })
+      .catch(error => { console.log('cdm katrina whoops', error)})
 
     axios.get('https://api.github.com/users/abqkatrina/followers')
-    .then(response => {
-      console.log('cdm follower response', response.data);
-      this.setState({followers: response.data})
-    })
-    .catch(error => { console.log('cdm whoops', error)})
+      .then(response => {
+        // console.log('cdm follower response', response.data);
+        this.setState({followers: response.data})
+
+
+      this.state.followers.map(follower => {
+        let username = follower.login;
+        axios.get(`https://api.github.com/users/${username}`)
+             .then(response => { 
+              //  console.log(response.data)
+                this.setState({ follower: response.data})
+                console.log('follower', this.state.follower)
+              })
+        })
+      })
+     
   };
 
 
@@ -37,7 +52,7 @@ class App extends Component {
 
   //  componentDidUpdate(prevProps, prevState) {
   //   if(prevState.user !== this.state.user){
-  //     axios.get('https://api.github.com/users/${this.state.query}')
+  //     axios.get('https://api.github.com/search/users?q=tom')
   //         .then(response => {
   //           console.log(response);
   //           this.setState({ user: response.data})
@@ -47,9 +62,10 @@ class App extends Component {
   // };
 
 
-  handleChanges = event => {
-    this.setState({ query: event.target.value})
-  };
+  // handleChanges = event => {
+  //   this.setState({ query: event.target.value})
+  // };
+
   render() {
     return (
       <div className="App">
@@ -58,12 +74,12 @@ class App extends Component {
         </header>
         <div className='App-main'>
           <form>
-            <input type='text' name='search' value={this.state.query} onChange={this.handleChanges} placeholder='search users'/>
+            <input type='text' name='search' /*value={this.state.query} onChange={this.handleChanges}*/ placeholder='search users'/>
             <button type='submit' /*onClick={this.componentDidUpdate}*/>search</button>
           </form>
-          <Card users={this.state.user}/>
-          {this.state.followers.map(follower => {
-          return (<Follower key={follower.login} follower={follower}/>)})}
+           <Card users={this.state.user}/>
+           <Card  key={this.state.follower.id} users={this.state.follower}/>
+          })}
         </div>
       </div>
     );
--- user-card/src/components/Follower.js
+++ user-card/src/components/Follower.js
@@ -16,7 +16,7 @@ const Follower = props => {
                     <p>Profile: <a href={props.follower.html_url}>{props.follower.html_url}</a></p>
                     <p>Organizations: <a href={props.follower.organizations_url}>{props.follower.organizations_url}</a></p>
                     <p>Followers: <a href={props.follower.followers_url}>{props.follower.followers_url}</a></p>
-                    <p>Fallowing: <a href={props.follower.following_url}>{props.follower.following_url}</a></p>
+                    <p>Following: <a href={props.follower.following_url}>{props.follower.following_url}</a></p>
                     {/* <p>Location: {props.follower.location}</p>
                     <p>Followers: {props.follower.followers}</p>
                     <p>Following: {props.follower.following}</p>
